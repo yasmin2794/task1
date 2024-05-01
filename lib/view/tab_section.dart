4df16887c0425a0b3846f926f8da1df6356a8ca4
yasmin2794 <yasmin2794@gmail.com>
2024-05-01 18:51:56 +0530

    commit

---
+++ lib/view/tab_section.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import '../model/app_state_model.dart';
+
+class TabSection extends StatelessWidget {
+  String sectionname;
+  TabSection({required this.sectionname});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = Provider.of<AppStateModel>(context, listen: true);
+
+    return Center(
+      child: Text('$sectionname section'),
+    );
+  }
+}
